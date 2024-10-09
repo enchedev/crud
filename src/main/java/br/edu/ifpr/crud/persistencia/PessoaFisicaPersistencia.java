4d6f1bc6e326dc3f18085009c0cf6f5367b3f752
nyyakko <nyakonyns@gmail.com>
2024-10-09 20:23:57 -0300

    Keep consistent naming inside the crud package

--- src/main/java/br/edu/ifpr/crud/persistencia/PessoaFisicaPersistencia.java
+++ src/main/java/br/edu/ifpr/crud/persistencia/PessoaFisicaPersistencia.java
@@ -3,6 +3,6 @@ package br.edu.ifpr.crud.persistencia;
 import br.edu.ifpr.crud.modelo.PessoaFisica;
 import br.edu.ifpr.persistencia.Persistence;
 
-public class PessoaFisicaPersistence extends Persistence<PessoaFisica> {
+public class PessoaFisicaPersistencia extends Persistence<PessoaFisica> {
 
 }
--- src/main/java/br/edu/ifpr/crud/persistencia/PessoaJuridicaPersistencia.java
+++ src/main/java/br/edu/ifpr/crud/persistencia/PessoaJuridicaPersistencia.java
@@ -3,6 +3,6 @@ package br.edu.ifpr.crud.persistencia;
 import br.edu.ifpr.crud.modelo.PessoaJuridica;
 import br.edu.ifpr.persistencia.Persistence;
 
-public class PessoaJuridicaPersistence extends Persistence<PessoaJuridica> {
+public class PessoaJuridicaPersistencia extends Persistence<PessoaJuridica> {
 
 }
